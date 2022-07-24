ce37607cdeb06ef9cd6b9a80cf99f1d11a135886
SallyXie <zxc88633@gmail.com>
2022-07-24 16:13:29 +0800

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Sally Xie on 2022/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeShop
+//
+//  Created by Sally Xie on 2022/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
